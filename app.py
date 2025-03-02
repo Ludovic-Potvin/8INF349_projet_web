c0027bffdc60144d2ea523bc9e91ed918f1bcdde
Ludovic Potvin <89479417+Ludovic-Potvin@users.noreply.github.com>
2025-03-02 17:53:38 -0500

    Fix launch issue and add logging correctly (#18)
    * Fix launch issue and add logging correectly
    
    * add log folder
    
    * add newline at end

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
-from app import create_app
+import json
+
+import logging.config
+import os
+
+from flask import Flask
+
 from app.database import init_db
+from config import Config
 
-app = create_app()
-
+app = Flask(__name__)
+app.config.from_object(Config)
 init_db()
+
+logger = logging.getLogger(app.config['LOGGER_NAME'])
+
+
+def setup_logging():
+    """Set up the logger configuration."""
+    config_file = app.config['LOGGER_CONFIG_FILE']
+
+    if not os.path.exists(app.config['LOG_DIR']):
+        os.makedirs(app.config['LOG_DIR'])
+
+    with open(config_file) as f_in:
+        config = json.load(f_in)
+    logging.config.dictConfig(config)
 
 @app.route('/')
 def hello_world():  # todo put application's code here
+    app.logger.info('Hello World')
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    setup_logging()
+    app.run(use_reloader=False)
--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'data.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SOURCE_API_URL = "https://dimensweb.uqac.ca/~jgnault/shops/products/"
+    LOGGER_NAME = "my_app"
+    LOGGER_CONFIG_FILE = os.path.join(BASE_DIR, 'logging', 'config.json')
+    LOG_DIR = os.path.join(BASE_DIR, 'logs')
