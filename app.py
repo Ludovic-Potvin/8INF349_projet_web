8a0fa5645a570613492f65467bedf04e91d596f3
Louis <louis.seranne@gmail.com>
2025-03-02 17:15:09 -0500

    Ajout classe

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import request, jsonify, make_response
 
 from app import create_app
 from app.database import init_db
-from app.controllers.post_order import *
+from app.controllers.order_controller import OrderController
 
 app = create_app()
 
@@ -17,12 +17,8 @@ def post_order():
     data = request.get_json()
     error_code = 200
     return_object = {"message": "Commande traitée avec succès"}
-
-    product = data.get('product', {})
-    id = product.get('id', {})
-    quantity = product.get('quantity', {})
-
-    return_object, error_code = process_order(product, id, quantity)
+    
+    return_object, error_code = OrderController.process_order(data)
     return return_object, error_code
 
 if __name__ == '__main__':
