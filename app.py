cf3ac7adc4508654a82e8a444be1972876d880c7
Louis <louis.seranne@gmail.com>
2025-03-02 13:45:30 -0500

    Check command validity

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from flask import request
+from flask import jsonify
+
 from app import create_app
 from app.database import init_db
 
@@ -8,6 +11,25 @@ init_db()
 @app.route('/')
 def hello_world():  # todo put application's code here
     return 'Hello World!'
+
+@app.route('/order', methods=['POST'])
+def post_order():
+    data = request.get_json()
+    if not 'product' in data or 'id' not in data['product'] or 'quantity' not in data['product'] or data['product']['quantity'] > 1 :
+        return jsonify({
+                        "errors" : {
+                            "product": {
+                                "code": "Missing-fields",
+                                "name": "La création d'une commande nécessite un produit"
+                            }
+                        }
+                    }), 422
+    
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
