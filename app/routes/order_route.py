8efb786686a58e1d3c21b128eb157e0ee6b84e37
Louis <louis.seranne@gmail.com>
2025-03-06 01:32:57 -0500

    Correction route

--- app/routes/order_route.py
+++ app/routes/order_route.py
@@ -7,15 +7,14 @@ order = Blueprint('order', __name__, url_prefix='/order')
 @order.route('/', methods=['POST'])
 def post_order():
     data = request.get_json()
-        
+
     return_object, error_code = OrderController.process_order(data)
     return return_object, error_code
 
-@order.route('/', methods=['GET'])
-def get_order():
-    data = request.get_json()
+@order.route('/<int:order_id>', methods=['GET'])
+def get_order(order_id: int):
     error_code = 200
     return_object = {"message": "Commande traitée avec succès"}
     
-    return_object, error_code = OrderController.get_order(data)
+    return_object, error_code = OrderController.get_order(order_id)
     return return_object, error_code
