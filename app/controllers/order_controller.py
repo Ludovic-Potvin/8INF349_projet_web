0dd04a6a92c462b17c54cdaa6971ff124bbc994f
Louis <louis.seranne@gmail.com>
2025-03-06 01:24:17 -0500

    Ajout Route + MAJ OrderController

--- app/controllers/order_controller.py
+++ app/controllers/order_controller.py
@@ -1,17 +1,26 @@
-from app.database import retrieve_product
+import app
+from app.controllers.product_controller import ProductController
+from app.database import Session
+from flask import abort
+
+from app.models.order import Order
+
 
 class OrderController():
 
     @classmethod
     def process_order(data):
+        error_code = 200
+        return_object = {"message": "Commande traitée avec succès"}
+
         product = data.get('product', {})
         id = product.get('id', {})
         quantity = product.get('quantity', {})
         
         if product and id and quantity and quantity >= 1 :
-            product = retrieve_product(id)
+            product = ProductController.get_product_by_id(id)
             if product and quantity <= product.in_stock:
-                #save order here
+                OrderController._saveorder(product, quantity)
             else:
                 error_code = 422
                 return_object = {
@@ -33,3 +42,41 @@ class OrderController():
                                 }
                             }
         return return_object, error_code
+    
+    @classmethod
+    def get_order(order_id: int):
+        with Session() as session:
+            try:
+                order = session.query(Order).filter(Order.id == order_id).first()
+                if order is None:
+                    abort(404, f"Order {order_id} not found")
+            except Exception as e:
+                app.logger.error(f"An error occurred: {str(e)}")
+                abort(500, "An unexpected server error happened")
+            finally:
+                session.close()
+        return order.to_dict
+    
+    @classmethod
+    def _saveorder(product, quantity_ordered):
+            with Session() as session:
+                try:
+                    # Création de la commande
+                    new_order = Order(
+                        product_id=product.id,
+                        quantity = quantity_ordered
+                    )
+
+                    # Mise à jour du stock du produit
+                    product.in_stock -= quantity_ordered
+
+                    # Sauvegarde dans la base de données
+                    session.add(new_order)
+                    session.commit()
+
+                    app.logger.info(f"Commande enregistrée avec succès : {new_order.id}")
+                except Exception as e:
+                    app.logger.error(f"An error occurred: {str(e)}")
+                    abort(500, "An unexpected server error happened")
+                finally:
+                    session.close()
---
+++ app/routes/order_route.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request
+
+from app.controllers.order_controller import OrderController
+
+order = Blueprint('order', __name__, url_prefix='/order')
+
+@order.route('/', methods=['POST'])
+def post_order():
+    data = request.get_json()
+        
+    return_object, error_code = OrderController.process_order(data)
+    return return_object, error_code
+
+@order.route('/', methods=['GET'])
+def get_order():
+    data = request.get_json()
+    error_code = 200
+    return_object = {"message": "Commande traitée avec succès"}
+    
+    return_object, error_code = OrderController.get_order(data)
+    return return_object, error_code
