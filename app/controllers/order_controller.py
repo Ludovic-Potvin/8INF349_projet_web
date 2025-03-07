89f24b4aead220d4879bb4153131fd763f7b3df0
Jérémie <suisserigert2003@hotmail.fr>
2025-03-06 22:00:13 -0500

    Added a try catch to change the location dynamically

--- app/controllers/order_controller.py
+++ app/controllers/order_controller.py
@@ -112,8 +112,12 @@ class OrderController():
                 session.commit()
 
                 app.logger.info(f"Commande enregistrée avec succès : {new_order.id}")
-                location = f"http://127.0.0.1:5000/order/{new_order.id}"
-                #location = url_for('order.get_order', order_id=new_order.id, _external=True)
+
+                try:
+                    location = url_for('order.get_order', order_id=new_order.id, _external=True)
+                except Exception as e:
+                    location = f"http://127.0.0.1:5000/order/{new_order.id}"
+
                 return_object = "Location : " + location
                 error_code = 201
             except Exception as e:
