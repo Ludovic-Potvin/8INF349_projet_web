b9d124476b7fc1b9d31b3c05f97fd518294b253f
Laurie-Anne L <102762236+LaurieAnneuh@users.noreply.github.com>
2025-04-22 22:10:29 -0400

    temp

--- app/routes/interface_route.py
+++ app/routes/interface_route.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 from app.controllers.product_controller import ProductController
 from app.controllers.order_controller import OrderController
+#from app.panier_helper import get_panier
 
 page = Blueprint('page', __name__, url_prefix='/page')
 
@@ -15,14 +16,10 @@ def get_product_page(product_id: int):
     product = ProductController.get_product_by_id(product_id)
     return render_template('product.html', product=product)
 
-@page.route('/panier/<int:order_id>', methods=['GET'])
-def get_panier(order_id: int):
-    return_object = {"message": "Commande traitée avec succès"}
-    
-    return_object, error_code = OrderController.get_order(order_id)
-    products = ProductController.get_product_by_id(return_object.product_id) #TO-DO CHANGE THIS TO RETURN MULTIPLE PRODUCTS
-    print(products)
-    return render_template('panier.html', order=return_object, products=products), error_code
+@page.route('/panier/<int:panier_id>', methods=['GET'])
+def get_panier(panier_id: int):
+    #products = get_panier(panier_id)
+    return render_template('panier.html')
 
 @page.route('/shipping_form/<int:id>', methods=['GET'])
 def shipping_form(id: int):
@@ -36,5 +33,5 @@ def paiement_form(id: int):
 @page.route('/confirmation/<int:id>', methods=['GET'])
 def confirmation(id: int):
     return_object, error_code = OrderController.get_order(id)
-    products = ProductController.get_product_by_id(return_object.product_id) #TO-DO CHANGE THIS TO RETURN MULTIPLE PRODUCTS
+    products = [ProductController.get_product_by_id(id) for id in return_object.products]
     return render_template('confirmation.html', id=id, order=return_object, products=products), error_code
